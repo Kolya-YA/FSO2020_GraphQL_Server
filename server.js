dd2cbc5d7cd52cf511334084c713bc770eca3789
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-11-18 16:57:29 +0100

    8.5: Books by genre

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,7 +107,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (parent, args) => {
+      let res = books
+      if (args.author) res = res.filter(b => b.author === args.author)
+      if (args.genre) res = res.filter(b => b.genres.some(g => g === args.genre))
+      return res
+      
+    },
     allAuthors: () => authors,
   },
   Book: {
